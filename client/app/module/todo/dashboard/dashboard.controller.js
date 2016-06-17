ee86af3fd840380975ded6545a759be39f6ea33d
Ashish-Lamse <ashish.lamse@india.semanticbits.com>
2016-06-17 16:16:31 +0530

    Committing to my local feature branch

--- client/app/module/todo/dashboard/dashboard.controller.js
+++ client/app/module/todo/dashboard/dashboard.controller.js
@@ -5,10 +5,13 @@
 (function(){
     angular
         .module('todoApp')
-        .controller('dashboardController',function(){
-
-
-    })
+        .controller('dashboardController',function ($resource) {
+            return $resource('http://192.168.2.33', {});
+        })
 
 }());
 
+/*.factory('Employee', ['$resource',
+    function ($resource) {
+        return $resource('http://10.0.1.24:3000/employees/:employeeId', {});
+    }])*/
--- client/app/module/todo/dashboard/dashboard.factory.js
+++ client/app/module/todo/dashboard/dashboard.factory.js
@@ -1,3 +1,16 @@
 /**
  * Created by Ashish Lamse on 16/6/16.
  */
+(function(){
+    angular.module('todoapp',[])
+        .factory('dashboardFactory',function ($resource) {
+            return $resource('http://192.168.2.33', {});
+        })
+}())
+
+
+/*
+factory('Employee', ['$resource',
+    function ($resource) {
+        return $resource('http://10.0.1.24:3000/employees/:employeeId', {});
+    }])*/
