4771319dfcb05b5bdb11cdcba80c0b823a1a8322
Ashish-Lamse <ashish.lamse@india.semanticbits.com>
2016-06-21 20:43:12 +0530

    Committing to my local feature branch date 20/06/2016

--- client/app/module/todo/dashboard/dashboard.factory.js
+++ client/app/module/todo/dashboard/dashboard.factory.js
@@ -1,16 +1,49 @@
 /**
  * Created by Ashish Lamse on 16/6/16.
  */
+
 (function(){
-    angular.module('todoapp',[])
-        .factory('dashboardFactory',function () {
+    angular
+        .module('todoApp')
+        .factory('dashboardFactory',function($http,$rootScope){
+            return{
+                readTask:function(){
+                    $rootScope.ALL_TASKS=null;
 
-        })
-}())
+                    $http.get('todotasks.json').success(function (response) {
+
+                        $rootScope.ALL_TASKS= response.todotasks;
 
 
-/*
-factory('Employee', ['$resource',
-    function ($resource) {
-        return $resource('http://10.0.1.24:3000/employees/:employeeId', {});
-    }])*/
+                    })
+                    return $rootScope.ALL_TASKS;
+                }
+            }
+        });
+}());
+
+
+
+
+/*dashboardFactory.$inject=['$http','$rootScope']
+
+
+ function  dashboardFactory($http,$rootScope) {
+ alert('inside factory')
+
+ $rootScope.ALL_TASKS=null;
+
+ var factory = {
+ readTask: readTask
+ };
+
+ return factory;
+ }
+
+
+ function readTask() {
+ $http.get('todotasks.json').success(function (response) {
+ $rootScope.ALL_TASKS = response.todotasks;
+ return $rootScope.ALL_TASKS;
+ });
+ }*/
